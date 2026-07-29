0eb96ef48c5a376fa98aa4be52880aa965daeeda
Jose Pacelli <josepacelli@gmail.com>
2026-07-29 18:46:27 -0300

    chore: bump version to 0.0.2
    - Update versionCode to 2
    - Update versionName to 0.0.2
    - Update download link on landing page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.github.josepacelli.opendisplay"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
     }
 
     signingConfigs {
