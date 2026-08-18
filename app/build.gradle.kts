ecf238953fec396df0e8031fae363a6fc4126de2
Jose Pacelli <josepacelli@gmail.com>
2026-08-18 12:53:07 -0300

    chore: bump version to 0.0.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.github.josepacelli.opendisplay"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.0.15"
+        versionCode = 16
+        versionName = "0.0.16"
     }
 
     signingConfigs {
