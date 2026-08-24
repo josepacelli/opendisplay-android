de812b9941d141292cf66b3cddedb2833363fed4
Jose Pacelli <josepacelli@gmail.com>
2026-08-24 10:25:00 -0300

    Bump to v0.0.30: Settings toggle to disable auto Picture-in-Picture
    PiP (v0.0.29) now defaults on but can be turned off in Settings if
    someone doesn't want the app auto-floating when they leave it
    connected (#53).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.github.josepacelli.opendisplay"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29
-        versionName = "0.0.29"
+        versionCode = 30
+        versionName = "0.0.30"
     }
 
     signingConfigs {
