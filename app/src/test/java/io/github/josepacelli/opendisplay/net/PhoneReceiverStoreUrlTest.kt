356db84c07d567b9d435f407cf8a31691578d33d
Jose Pacelli <josepacelli@gmail.com>
2026-08-24 16:11:26 -0300

    test(components): corrigir teste para rejeitar esquema não https

--- app/src/test/java/io/github/josepacelli/opendisplay/net/PhoneReceiverStoreUrlTest.kt
+++ app/src/test/java/io/github/josepacelli/opendisplay/net/PhoneReceiverStoreUrlTest.kt
@@ -17,7 +17,7 @@ class PhoneReceiverStoreUrlTest {
     @Test
     fun `rejects a non-https scheme`() {
         assertNull(PhoneReceiver.sanitizedStoreUrl("http://github.com/x"))
-        assertNull(PhoneReceiver.sanitizedStoreUrl("javascript:alert(1)"))
+        assertNull(PhoneReceiver.sanitizedStoreUrl("javascript:alert(1)"))de
     }
 
     @Test
